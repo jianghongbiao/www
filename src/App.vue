90b38b1773205078839d4d56af70b115cb26db87
jhb <2162447971@qq.com>
2023-04-11 15:48:06 +0800

    第1次提交代码

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+	<div>
+		<!-- 占位 -->
+		<router-view/>
+	</div>
+</template>
+
+<style>
+	
+</style>
---
+++ src/store/index.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+// 导入 axios
+import axios from 'axios';
+
+export default new Vuex.Store({
+	//state	存储共享的数据
+	state: {
+		
+	},
+	//计算属性，用于对state中的数据进行加工处理再返回
+	getters: {
+		
+		
+	},
+	// 用于改变共享数据
+	mutations: {
+		
+		
+	},
+	actions: {
+		
+		
+	},
+	modules: {}
+})
